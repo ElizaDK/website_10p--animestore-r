3f6d98a48e7dc8a7fd46c27016951c3a4a3a621a
Eliza César <elizaengtec@gmail.com>
2021-10-22 13:41:53 -0300

    package: atualizacao section lançamentos 2022

--- assets/js/script1.js
+++ assets/js/script1.js
@@ -31,3 +31,29 @@
       },
     },
   });
+//section arrivals
+  var swiper = new Swiper(".arrivals-slider", {
+    spaceBetween:10,
+    loop:true,
+    centeredSlides:true,
+    autoplay:{
+       delay:3400,
+       disableOnInteraction:false,
+   },
+   
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        
+      },
+     
+      768: {
+        slidesPerView: 2,
+      
+      },
+      1024: {
+        slidesPerView: 3,
+       
+      },
+    },
+  });
